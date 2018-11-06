62ef3e8654538f415b6ac42404513cf3592fa5a1
myGit.Conf <inchuuryuu@163.com>
2018-11-06 18:33:17 +0800

    Initial Commit

---
+++ ADT/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ADT
+//
+//  Created by redboy on 06/11/2018.
+//  Copyright © 2018 children. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
